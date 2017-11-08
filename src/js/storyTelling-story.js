7b4d7131a449eddfb19a1d6092035bdac4fb3695
BlueSlug <mossmonkey@gmail.com>
2017-11-08 17:06:38 -0500

    SJRK-19: added value to story model with description of intended use

--- src/js/storyTelling-story.js
+++ src/js/storyTelling-story.js
@@ -25,6 +25,7 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENS
             tags: [],
             categories: [],
             summary: "",
+            sharing: "", //flexible indicator of privacy/sharing status, e.g. "public", "private", "unlisted"
             requestedTranslations: [
                 //"es": 2, // a list of language codes as keys with
                 //"fr": 5  // the number of requests for that language
